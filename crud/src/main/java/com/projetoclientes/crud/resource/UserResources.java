d143a97956156ff2c7d1baf9a47d72c33c4193d5
Leonardo Marinho <leowalkg6@gmail.com>
2024-12-18 20:26:39 -0300

    creating User and Resource

---
+++ crud/src/main/java/com/projetoclientes/crud/resource/UserResources.java
@@ -0,0 +1,20 @@
+package com.projetoclientes.crud.resource;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.projetoclientes.crud.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResources {
+
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L,"Maria","maria@gmail.com","99999999","12345");
+		return ResponseEntity.ok().body(u);
+	}
+	
+}
