3823f28bf5e4cdd430aa829a9521e596592274a0
Leonardo Marinho <leowalkg6@gmail.com>
2025-01-01 21:51:12 -0300

    fix build spring app

--- crud/src/main/java/com/projetoclientes/crud/entities/User.java
+++ crud/src/main/java/com/projetoclientes/crud/entities/User.java
@@ -1,6 +1,7 @@
 package com.projetoclientes.crud.entities;
 
 import java.io.Serializable;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
